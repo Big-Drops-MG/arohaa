ee5374efccdf08bd60bd3e9981d8c25bf574eabb
neha-bigdrops <neha@bigdropsmarketing.com>
2026-04-27 19:28:50 +0530

    test

--- apps/dashboard/features/view/dashboard/HomePage.tsx
+++ apps/dashboard/features/view/dashboard/HomePage.tsx
@@ -1,7 +1,9 @@
 export function HomePage() {
   return (
     <div className="flex min-h-svh items-center justify-center">
-      <h1 className="text-2xl font-semibold">Welcome to Dashboard 🚀</h1>
+      <h1 className="text-2xl font-semibold">
+        Welcome to Dashboard of AROHAA🚀{" "}
+      </h1>
     </div>
   )
 }
